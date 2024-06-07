f20d5d4195a57b20b3295e37ad2c48e96562d460
rogsouza <radsouza@gmail.com>
2024-06-07 15:28:35 -0300

    first commit

---
+++ src/main/java/scraperPkg/Product.java
@@ -0,0 +1,48 @@
+package scraperPkg;
+
+public class Product {
+
+	private String url;
+	private String image;
+	private String name;
+	private String price;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "{ \"url\":\"" + url + "\", " + " \"image\": \"" + image + "\", " + "\"name\":\"" + name + "\", "
+				+ "\"price\": \"" + price + "\" }";
+	}
+
+}
